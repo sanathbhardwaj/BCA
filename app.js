f0aba705b82a432078bca99968dea4f57a38f327
RISHU GUPTA <guptarishu04@gmail.com>
2019-09-28 23:31:23 +0530

    home page

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+var bodyParser = require('body-parser');
+const app=express();
+app.use(require('./utils/cors'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(process.env.PORT||5000,()=>{
+    console.log("Servrt started.....5000");
+})
